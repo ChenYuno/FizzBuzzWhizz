fff09dc340958bcbc4387788c8dd1bc3926d94c0
YunChen <1183728554@qq.com>
2022-07-21 13:47:46 +0800

    feat:create test，should_output_Fizz_when_multiple_3_given_3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_output_Fizz_when_multiple_3_given_3() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String out  = fizzBuzz.countOff(3);
+
+        String except = "Fizz";
+        //then
+        assertThat(out).isEqualTo(except);
+    }
+
 }
