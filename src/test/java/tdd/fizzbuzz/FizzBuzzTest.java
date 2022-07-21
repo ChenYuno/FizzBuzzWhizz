bd0e4abcce30fabacbea0daf2646b8e11c10adfd
YunChen <1183728554@qq.com>
2022-07-21 14:00:21 +0800

    feat:create test should_output_Buzz_when_multiply_of_5_given_5

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -8,15 +8,26 @@ public class FizzBuzzTest {
 
 
     @Test
-    void should_output_Fizz_when_multiple_3_given_3() {
+    void should_output_Fizz_when_multiple_of_3_given_3() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
         //when
         String out  = fizzBuzz.countOff(3);
-
-        String except = "Fizz";
         //then
+        String except = "Fizz";
         assertThat(out).isEqualTo(except);
     }
+
+    @Test
+    void should_output_Buzz_when_multiply_of_5_given_5() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String output = fizzBuzz.countOff(5);
+        //then
+        String except = "Buzz";
+        assertThat(output).isEqualTo(except);
+    }
+
 
 }
