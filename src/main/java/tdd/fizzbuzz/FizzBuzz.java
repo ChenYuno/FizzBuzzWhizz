914d6a921552689da1f5f6ef22175a7840907260
YunChen <1183728554@qq.com>
2022-07-21 16:17:37 +0800

    feat:finish default data transform when it is contain 3,5 or 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,27 +1,53 @@
 package tdd.fizzbuzz;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang.StringUtils;
 import tdd.util.Constant;
 
 
 public class FizzBuzz {
 
+    @Getter
+    @Setter
+    private class Index{
+        private int currIdx;
+
+        public void resetIdx() {
+            currIdx = 0;
+        }
+    }
     public String countOff(int num) {
         String reData = "";
         reData += isMultiplyOfThree(num) ? Constant.FIZZ : "";
         reData += isMultiplyOfFive(num) ? Constant.BUZZ : "";
         reData += isMultiplyOfSeven(num) ? Constant.WHIZZ : "";
-        return reData.equals("") ? String.valueOf(num) : reData;
+        if (!StringUtils.isEmpty(reData)) {
+            return reData;
+        } else {
+            StringBuilder stringBuilder = new StringBuilder(reData);
+            isContain357(num, stringBuilder);
+            return StringUtils.isEmpty(stringBuilder.toString()) ?
+                    String.valueOf(num) :
+                    stringBuilder.toString();
+        }
     }
 
-    private boolean isContainThree(int num) {
-        return false;
+
+    private void isContain357(int num, StringBuilder reDate) {
+        String data = String.valueOf(num);
+        for (int idx = 0; idx < data.length(); idx++) {
+            char currNum = data.charAt(idx);
+            if (currNum == '3') {
+                reDate.append(Constant.FIZZ);
+            } else if (currNum == '5') {
+                reDate.append(Constant.BUZZ);
+            } else if (currNum == '7') {
+                reDate.append(Constant.WHIZZ);
+            }
+        }
     }
-    private boolean isContainFive(int num) {
-        return false;
-    }
-    private boolean isContainSeven(int num) {
-        return false;
-    }
+
 
     private boolean isMultiplyOfThree(int num) {
         return num % 3 == 0;
